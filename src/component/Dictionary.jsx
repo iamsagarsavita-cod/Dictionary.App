400ade12facf12ef82139d6d24ae1ab93cfc0f34
iamsagarsavita-cod <iamsagarsavita@gmail.com>
2026-07-15 23:53:21 +0530

    I completly change the Ui basic to advance

--- src/component/Dictionary.jsx
+++ src/component/Dictionary.jsx
@@ -1,17 +1,16 @@
-import React, { use } from 'react'
-import { useState } from 'react';
-import axios from 'axios'
+import React, { use } from "react";
+import { useState } from "react";
+import axios from "axios";
 
 const Dictionary = () => {
-
   const [word, setWord] = useState("");
   const [loading, setLoading] = useState(false);
   const [errors, setErrors] = useState("");
   const [wordData, setWordData] = useState(null);
 
   const searchWord = async () => {
-    try{
-      if(word.trim().length === 0){
+    try {
+      if (word.trim().length === 0) {
         setErrors("Please Enter a Word...");
         return;
       }
@@ -24,83 +23,102 @@ const Dictionary = () => {
       );
       console.log(res);
       setWordData(res.data[0]);
-    }catch (error){
+    } catch (error) {
       console.log(error);
       setErrors("word Not Found");
-    }finally{
+    } finally {
       setLoading(false);
     }
-  }
+  };
 
+  const clearSearch = () => {
+    setWord("");
+    setWordData(null);
+    setErrors("");
+  };
 
   return (
-    <div className="min-h-screen bg-linear-to-r from-indigo-100 via-sky-100 to-cyan-100 flex justify-center items-center">
-      <div className="bg-white/50 w-full max-w-2xl rounded-3xl p-8 shadow-xl">
-      <h1 className="text-4xl text-center font-bold text-indigo-600 mb-8">
-        📖 Dictionary app</h1>
+    <div className="min-h-screen bg-gradient-to-br from-blue-100 via-indigo-100 to-purple-100 flex justify-center items-center">
+      <div className="bg-white w-full max-w-2xl rounded-3xl p-8 shadow-2xl border border-indigo-100">
+        <h1 className="text-5xl text-center font-bold text-indigo-600 mb-8">
+          📖 Dictionary app
+        </h1>
 
         <div className="flex gap-3">
-
-          <input type="text" placeholder="Enter any word" className="flex-1 border-2 border-indigo-300 rounded-xl px-4 py-3 outline-none  focus:border-indigo-600" 
-          value={word}
-          onChange={(e) => setWord(e.target.value)}
+          <input
+            type="text"
+            placeholder="🔍 Enter a word..."
+            className="flex-1 px-5 py-3 border border-indigo-300 rounded-xl outline-none focus:ring-2 focus:ring-indigo-400"
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                searchWord();
+              }
+            }}
           />
 
           <button
-          className="bg-indigo-600 text-white px-10 font-medium text-lg rounded-xl hover:bg-indigo-700 cursor-pointer"
-          onClick={searchWord}>
-            Search</button>
+            className="bg-indigo-600 hover:bg-indigo-700 transition-all duration-300 text-white px-8 rounded-xl font-semibold shadow-lg cursor-pointer"
+            onClick={searchWord}
+          >
+            Search
+          </button>
         </div>
 
-        {
-          loading && (
-            <h2 className=" text-center mt-6 text-lg text-red-800 font-semibold animate-pulse">
-              Searching....
-            </h2>
-          )
-        }
+        {loading && (
+          <h2 className=" text-center mt-6 text-lg text-red-800 font-semibold animate-pulse">
+            Searching....
+          </h2>
+        )}
 
-        {
-          errors && (
-            <h2 className="text-center mt-6 text-lg text-red-800 font-semibold">
-              {errors}
-            </h2>
-          )
-        }
+        {errors && (
+          <h2 className="text-center mt-6 text-lg text-red-800 font-semibold">
+            {errors}
+          </h2>
+        )}
 
-        {
-          wordData && (
-            <div className='mt-8 bg-indigo-50 rounded-2xl p-6'>
-              <h1 className='text-3xl font-bold text-indigo-700'>
-                {wordData.word}
-              </h1>
-              <p className='text-gray-700 mt-1'>
-                  {wordData.phonetic || "phonetic not found"}
-              </p>
-              <div className='m-5 space-y-3'>
-                <div className='bg-white/70 rounded-xl p-3 flex justify-between'>
-                  <span className='font-semibold'>part of Speech</span>
-                  <span>{wordData.meanings[0].partOfSpeech}</span>
-                </div>
+        {wordData && (
+          <div className="mt-8 bg-blue-200 rounded-2xl shadow-lg border border-indigo-100 p-6">
+            <h1 className="text-3xl font-bold text-indigo-700">
+              {wordData.word.charAt(0).toUpperCase() + wordData.word.slice(1)}
+            </h1>
+            <p className="text-gray-700  italic mt-2">
+              🔊 {wordData.phonetic || "Phonetic not available"}
+            </p>
+            <div className="m-5 space-y-3">
+              <div className="bg-indigo-100 rounded-xl p-4">
+                <h3 className="font-semibold text-indigo-700">
+                  Part of Speech
+                </h3>
 
-                <div className='bg-white/70 rounded-xl p-3'>
-                  <h3 className='font-semibold mb-2'> Meaning</h3>
-                  <p>{wordData.meanings[0].definitions[0].definition}</p>
-                </div>
-
-                <div className='bg-white/70 rounded-xl p-3'>
-                  <h3 className='font-semibold mb-2'> Example</h3>
-                  <p>{wordData.meanings[0].definitions[0].example || "No Example Available"}</p>
-                </div>
+                <p className="text-lg">
+                  {wordData.meanings?.[0]?.partOfSpeech}
+                </p>
               </div>
 
-            </div>
-          )
-        }
+              <div className="bg-green-50 rounded-xl p-4 mt-4">
+                <h3 className="font-semibold text-green-700">Meaning</h3>
 
+                <p className="mt-2">
+                  {wordData.meanings?.[0]?.definitions?.[0]?.definition}
+                </p>
+              </div>
+
+              <div className="bg-yellow-50 rounded-xl p-4 mt-4">
+                <h3 className="font-semibold text-yellow-700">Example</h3>
+
+                <p className="italic mt-2">
+                  {wordData.meanings?.[0]?.definitions?.[0]?.example ||
+                    "No example available"}
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Dictionary;
