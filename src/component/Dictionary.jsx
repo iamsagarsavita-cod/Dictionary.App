427ab03dd4491f3aa233b17418a88f067456d311
iamsagarsavita-cod <iamsagarsavita@gmail.com>
2026-07-16 00:11:54 +0530

    added a new cross button

--- src/component/Dictionary.jsx
+++ src/component/Dictionary.jsx
@@ -62,7 +62,7 @@ const Dictionary = () => {
             {word && (
               <button
                 onClick={clearSearch}
-                className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-red-500 text-xl font-bold"
+                className="absolute right-3 top-1/2 -translate-y-1/2 text-xl"
               >
                 ✖
               </button>
