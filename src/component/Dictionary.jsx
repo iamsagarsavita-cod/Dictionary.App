0e8c6925438ff20901ab1394460a05f6e7a8e032
iamsagarsavita-cod <iamsagarsavita@gmail.com>
2026-07-19 23:32:32 +0530

    adding the dark and light mode

--- src/component/Dictionary.jsx
+++ src/component/Dictionary.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import axios from "axios";
+import { FaMoon, FaSun } from "react-icons/fa";
 import AudioPlayer from "./AudioPlayer";
 
 const Dictionary = () => {
@@ -8,6 +9,8 @@ const Dictionary = () => {
   const [loading, setLoading] = useState(false);
   const [errors, setErrors] = useState("");
   const [wordData, setWordData] = useState(null);
+
+  const [darkMode, setDarkMode] = useState(false);
 
   // Audio ke liye
   const audioUrl = wordData?.phonetics?.find((item) => item.audio)?.audio;
@@ -43,26 +46,55 @@ const Dictionary = () => {
 
   // sysnonyms ke liye ek flapMap banake duplicate remove hoge
   const synonyms = [
-  ...new Set(
-    wordData?.meanings?.flatMap(
-      (meaning) => meaning.synonyms || []
-    ) || []
-  ),
-];
+    ...new Set(
+      wordData?.meanings?.flatMap((meaning) => meaning.synonyms || []) || [],
+    ),
+  ];
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-100 via-indigo-100 to-purple-100 flex justify-center items-center">
-      <div className="bg-white w-full max-w-2xl rounded-3xl p-8 shadow-2xl border border-indigo-100">
-        <h1 className="text-5xl text-center font-bold text-indigo-600 mb-8">
-          📖 Dictionary app
-        </h1>
+    // Outer BOX
+    <div
+      className={`min-h-screen flex justify-center items-center transition-all duration-500 ${
+        darkMode
+          ? "bg-gray-900"
+          : "bg-gradient-to-br from-blue-100 via-indigo-100 to-purple-100"
+      }`}
+    >
+      {/* Card BOX */}
+
+      <div
+        className={`w-full max-w-2xl rounded-3xl p-8 shadow-2xl transition-all duration-500 ${
+          darkMode
+            ? "bg-gray-800 text-white border-gray-700"
+            : "bg-white border-indigo-100"
+        }`}
+      >
+        {/* Heading  */}
+
+        <div className="flex justify-between items-center mb-8">
+          <h1 className="text-5xl font-bold text-indigo-600">
+            📖 Dictionary App
+          </h1>
+
+          <button
+            onClick={() => setDarkMode(!darkMode)}
+            className="bg-indigo-600 text-white p-3 rounded-full hover:scale-110 transition"
+          >
+            {darkMode ? <FaSun size={15} /> : <FaMoon size={15} />}
+          </button>
+        </div>
 
         <div className="flex gap-3">
           <div className="relative flex-1">
             <input
               type="text"
               placeholder="Enter any word..."
-              className="w-full border-2 border-indigo-300 rounded-xl px-4 py-3 pr-10 outline-none focus:border-indigo-600"
+              className={`w-full border-2 rounded-xl px-4 py-3 pr-10 outline-none transition-all duration-300
+              ${
+                darkMode
+                  ? "bg-gray-700 text-white border-gray-600 placeholder-gray-400 focus:border-indigo-400"
+                  : "bg-white text-black border-indigo-300 placeholder-gray-500 focus:border-indigo-600"
+              }`}
               value={word}
               onChange={(e) => setWord(e.target.value)}
               onKeyDown={(e) => {
@@ -83,8 +115,11 @@ const Dictionary = () => {
           </div>
 
           <button
-            onClick={searchWord}
-            className="bg-indigo-600 text-white px-8 rounded-xl hover:bg-indigo-700"
+            className={`px-8 rounded-xl text-white ${
+              darkMode
+                ? "bg-orange-500 hover:bg-orange-600"
+                : "bg-indigo-600 hover:bg-indigo-700"
+            }`}
           >
             Search
           </button>
@@ -118,7 +153,11 @@ const Dictionary = () => {
               🔊 {wordData.phonetic || "Phonetic not available"}
             </p>
             <div className="m-5 space-y-3">
-              <div className="bg-indigo-100 rounded-xl p-4">
+              <div
+                className={`rounded-xl p-4 ${
+                  darkMode ? "bg-gray-700" : "bg-indigo-100"
+                }`}
+              >
                 <h3 className="font-semibold text-indigo-700">
                   Part of Speech
                 </h3>
@@ -128,7 +167,11 @@ const Dictionary = () => {
                 </p>
               </div>
 
-              <div className="bg-green-50 rounded-xl p-4 mt-4">
+              <div
+                className={`rounded-xl p-4 mt-4 ${
+                  darkMode ? "bg-gray-700" : "bg-green-50"
+                }`}
+              >
                 <h3 className="font-semibold text-green-700">Meaning</h3>
 
                 <p className="mt-2">
@@ -136,7 +179,11 @@ const Dictionary = () => {
                 </p>
               </div>
 
-              <div className="bg-yellow-50 rounded-xl p-4 mt-4">
+              <div
+                className={`rounded-xl p-4 mt-4 ${
+                  darkMode ? "bg-gray-700" : "bg-yellow-50"
+                }`}
+              >
                 <h3 className="font-semibold text-yellow-700">Example</h3>
 
                 <p className="italic mt-2">
@@ -144,7 +191,11 @@ const Dictionary = () => {
                     "No example available"}
                 </p>
               </div>
-              <div className="bg-green-200 rounded-xl p-4 mt-4">
+              <div
+                className={`rounded-xl p-4 mt-4 ${
+                  darkMode ? "bg-gray-700" : "bg-green-200"
+                }`}
+              >
                 <h3 className="font-semibold text-red-500">Synonyms</h3>
 
                 <p className="italic mt-2">
